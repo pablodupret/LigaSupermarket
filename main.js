470838aededd0fa0604204af77889a27f4a45b21
Pablo Dupret <133659936+pablodupret@users.noreply.github.com>
2025-09-28 19:12:25 -0300

    Atualização NOVA RODADA
    Nona Rodada

--- main.js
+++ main.js
@@ -48,7 +48,8 @@ async function carregarJogos() {
 5: { data: "09/08/2025", draft: "Draft Edge of Ethernities"},
 6: { data: "23/08/2025", draft: "Foundations"},
 7: { data: "06/09/2025", draft: "Duskmourn"},
-8: { data: "20/09/2025", draft: "Draft Aetherdrift"}
+8: { data: "20/09/2025", draft: "Draft Aetherdrift"},
+9: { data: "28/09/2025", draft: "Pré-release Homem Aranha"}
 
 };
 
